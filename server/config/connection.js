ba159a4f7ae0e0522e1150d2cc2b89d53aa297c3
ctnohilly <ctnohilly@gmail.com>
2022-10-03 22:39:16 -0400

    login and signup working correctly

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/schools-db',
+  process.env.MONGODB_URI || 'mongodb://localhost/google-books-db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
